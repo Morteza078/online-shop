b633dd3c1a7621c2131edd2e11d88875ac45cda1
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-10 13:38:52 -0700

    add AUTH_USER_MODEL on settings.py for CustomUser

--- shop/settings.py
+++ shop/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+AUTH_USER_MODEL = 'customer.CustomUser'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
