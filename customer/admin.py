044c96df86fd702584829f8c743b0932069f3493
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-10 13:37:04 -0700

    add new models in admin.py files

--- customer/admin.py
+++ customer/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from customer.models import Customer,Address,DiscountCode
+from customer.models import Customer, Address, Staff,CustomUser
 
 admin.site.register(Customer)
 admin.site.register(Address)
-admin.site.register(DiscountCode)
+admin.site.register(Staff)
+admin.site.register(CustomUser)
--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from orders.models import OrderItem, Order, OrderHistory
+from orders.models import OrderItem, Order, OrderHistory, DiscountCode
 
 # Register your models here.
 
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(OrderHistory)
+admin.site.register(DiscountCode)
