a333b5385c74eb3fcf39bc82f305859d97be2d12
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-12 22:37:46 -0700

    Prepare a home page template

---
+++ products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from products.views import index
+
+app_name = 'products'
+
+urlpatterns = [
+    path('', index, name='index'),
+    # path('detail/<int:event_id>/', EventDetailView.as_view(), name='detail'),
+    # path('create/', create_or_update, name='create'),
+    # path('update/<int:event_id>/', create_or_update, name='update'),
+    # path('user-events/', user_event, name='user-events'),
+]
--- shop/settings.py
+++ shop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 AUTH_USER_MODEL = 'customer.CustomUser'
