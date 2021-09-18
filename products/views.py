6d0b76f2664bfed1b6ec568c95f4cc1fe8597269
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-18 08:42:35 -0700

    add change lang view for multi language site

--- products/views.py
+++ products/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.utils.translation import activate
+
 def index(request):
+    return render(request, 'products/index.html')
+
+
+def change_lang(request):
     if request.method == "GET":
-        return render(request, 'products/index.html')
+        print('in change_lang')
+        activate(request.GET.get('lang'))
+        return redirect(request.GET.get('next'))
