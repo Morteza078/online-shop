d6bfc75c34e9668d632008416a9d1a2d4f069d92
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-18 08:28:56 -0700

    add new view function for change password

---
+++ customer/forms.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from customer.models import Customer, CustomUser
+
+
+class CustomerLoginForm(AuthenticationForm):
+    class Meta:
+        model = Customer
+        fields = ('username', 'password')
+
+
+# class CustomerRegisterForm(UserCreationForm):
+#
+#     class Meta:
+#         model = Customer
+#         fields = ('username', 'password1', 'password2')
+class CustomerRegisterForm(forms.Form):
+    username = forms.CharField(max_length=100, label=_('username'))
+    email = forms.CharField(max_length=100, label=_('email'))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput, label=_('password'))
+    confirm_password = forms.CharField(max_length=100, widget=forms.PasswordInput, label=_('confirm password'))
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     password = cleaned_data.get('password')
+    #     confirm_password = cleaned_data.get('confirm_password')
+    #     username = cleaned_data.get('username')
+    #     # print(Customer.objects.filter(username=username).exists())
+    #     if CustomUser.objects.filter(username=username).exists():
+    #         raise forms.ValidationError(_('this username already exist'))
+    #     elif password != confirm_password or password is None or confirm_password is None:
+    #         raise forms.ValidationError(_('The two password fields didn’t match'))
+    #     return cleaned_data
+
+
+class CustomerChangePassword(PasswordChangeForm):
+    """
+    Inheritanced from Built-in Change Password Form
+    """
+
+    pass
