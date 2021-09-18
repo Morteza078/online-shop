e3dbbe66aa3e73cf34fd30a39a0ed0a41d2eb913
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-18 16:27:29 -0700

    some change in url of login view

--- customer/urls.py
+++ customer/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
-from .views import CustomerLoginView, CustomerLogoutView, ChangePasswordView, RegisterView, CustomerProfileView
+from .views import CustomerLoginView, CustomerLogoutView, ChangePasswordView, RegisterView, CustomerProfileView, \
+    SendLinkResetPasswordView,Set_New_Password
 
 app_name = 'customer'
 
 urlpatterns = [
-    path('auth/', CustomerLoginView.as_view(), name="auth"),
+    path('login/', CustomerLoginView.as_view(), name="login"),
     path('register/', RegisterView.as_view(), name="register"),
     path('logout/', CustomerLogoutView.as_view(), name="logout"),
-    path('change_password/', ChangePasswordView.as_view(), name="change_password"),
+    path('reset-password/', SendLinkResetPasswordView.as_view(), name="reset-password"),
+    path('set-new-password/<uidb64>/<token>/', Set_New_Password, name="set-new-password"),
+    path('change-password/', ChangePasswordView.as_view(), name="change_password"),
     path('profile/', CustomerProfileView.as_view(), name="profile"),
 ]
