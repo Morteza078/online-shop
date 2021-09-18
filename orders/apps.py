ab630da745d79a4d73477059a3faa9ddaaf5fb85
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-18 08:26:13 -0700

    change on some file and add some files

--- orders/apps.py
+++ orders/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+    verbose_name = _("orders")
