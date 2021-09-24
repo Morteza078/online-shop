3c5bee5b290071acfe01dead73270b1da78342b2
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-24 08:21:11 -0700

    add url for product by category

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from products.views import index
+from products.views import IndexView, product_by_category
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', index, name='index'),
-    # path('create/', create_or_update, name='create'),
-    # path('update/<int:event_id>/', create_or_update, name='update'),
-    # path('user-events/', user_event, name='user-events'),
+    path('', IndexView.as_view(), name='index'),
+    path('category_product/<int:pk>/', product_by_category, name='product_category'),
+
 ]
