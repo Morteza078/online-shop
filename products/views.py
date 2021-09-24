5f9fe5da78a6a2c6dc3572188b25c54649a51a3b
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-24 08:32:31 -0700

    add index view and product by category view

--- products/views.py
+++ products/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from django.utils.translation import activate
+from django.views import View
+from django.views.generic import DetailView
 
-def index(request):
-    return render(request, 'products/index.html')
+from products.models import Product, Advertising, Category
 
 
 def change_lang(request):
@@ -13,3 +14,16 @@ def change_lang(request):
         print('in change_lang')
         activate(request.GET.get('lang'))
         return redirect(request.GET.get('next'))
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        advertisings = Advertising.objects.all()
+        context = {'advertisings': advertisings, 'products': products}
+        return render(request, "products/index.html", context)
+
+
+def product_by_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'products/category_products.html', context={'category': category})
