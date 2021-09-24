53cca1356f1f6545acefbbca26cb9fc174a9811a
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-24 08:17:38 -0700

    add url for change password

--- customer/api/urls.py
+++ customer/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import RegisterAPIView, RequestPaswordResetEmail, CheckPasswordTokenView, SetNewPasswordAPIView
+from .views import RegisterAPIView, RequestPaswordResetEmail, CheckPasswordTokenView, SetNewPasswordAPIView, \
+    SetChangePasswordView
 
 urlpatterns = [
     path('', RegisterAPIView.as_view(), name='api-customer-list'),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('password-reset/<uidb64>/<token>/',
          CheckPasswordTokenView.as_view(), name='password-reset-confirm'),
     path('password-reset-complete/', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
+    path('set-change-password', SetChangePasswordView.as_view(), name='set-change-password'),
+    # path('edit-profile', EditProfileView.as_view(), name='edit-profile'),
 ]
