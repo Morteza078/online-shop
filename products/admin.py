d9b9009b9d559d5704e79f66ebc404e17ce36bc3
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-24 08:03:24 -0700

    add advertising model to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Attribute, Category, ProductImage
+from .models import Product, Attribute, Category, ProductImage,Advertising
 
 admin.site.register(Attribute)
 admin.site.register(Category)
 admin.site.register(ProductImage)
 admin.site.register(Product)
+admin.site.register(Advertising)
