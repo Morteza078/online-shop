bdff12121bd567753c8e88da542390915d11fd12
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-24 08:22:12 -0700

    add media url for displaying image to site

--- shop/urls.py
+++ shop/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from products.views import change_lang
@@ -32,5 +34,6 @@ urlpatterns += i18n_patterns(
 )
 
 urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
-]
+    path('api-auth/', include('rest_framework.urls')), ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
