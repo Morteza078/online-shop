c5b02e1bde2eac277d343e14fdc97ad6cf1034f0
morteza078 <mortezarobatjazi.77@gmail.com>
2021-09-24 08:20:43 -0700

    add url for pagination

--- products/api/urls.py
+++ products/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from products.api.views import PaginationView
 
+urlpatterns = [
+    path('', PaginationView.as_view(), name='api-product-list'),
+    # path('product-images', ProductImageView.as_view(), name='api-product-image'),
 ]
