910261fd0af269d45c2482c16d9ae3df21c9e6b2
morteza078 <mortezarobatjazi.77@gmail.com>
2021-10-01 12:01:22 -0700

    add total_price template tag for display total price with final price and total price in invoice.html

--- products/templatetags/base_tags.py
+++ products/templatetags/base_tags.py
@@ -13,3 +13,8 @@ def title():
 @register.inclusion_tag('products/category.html')
 def category():
     return {'categories': Category.objects.all()}
+
+
+@register.simple_tag
+def total_price(final_price, discount):
+    return float((100*final_price)/(100-discount))
