61b17e2f4b2ca37841238f8a3977e01d95ab9eb3
morteza078 <mortezarobatjazi.77@gmail.com>
2021-10-01 12:26:48 -0700

    create product detail view

--- products/views.py
+++ products/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+from django.urls import reverse
 from django.utils.translation import activate
 from django.views import View
 from django.views.generic import DetailView
 
+from orders.forms import CartForm
 from products.models import Product, Advertising, Category
 
 
@@ -26,3 +28,25 @@ class IndexView(View):
 def product_by_category(request, pk):
     category = get_object_or_404(Category, pk=pk)
     return render(request, 'products/category_products.html', context={'category': category})
+
+
+# class ProductDetailView(DetailView):
+#     """
+#         View for Customers See Product Detials and buy it
+#         """
+#     model = Product
+
+class ProductDetailView(View):
+    """
+     View for Customers See Product Detials and buy it
+    """
+
+    def get(self, request, pk, *args, **kwargs):
+        try:
+            product = Product.objects.get(id=pk)
+            form = CartForm()
+        except Product.DoesNotExist:
+            return redirect(reverse("products:index"))
+        return render(request, "products/product_detail.html", {
+            'product': product, 'form': form
+        })
