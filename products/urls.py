a80b3f09f90b38f38aed89655c04620178a26183
morteza078 <mortezarobatjazi.77@gmail.com>
2021-10-01 12:24:48 -0700

    add url for displaying of product detail

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from products.views import IndexView, product_by_category
+from products.views import IndexView, product_by_category,ProductDetailView
 
 app_name = 'products'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('category_product/<int:pk>/', product_by_category, name='product_category'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 
 ]
