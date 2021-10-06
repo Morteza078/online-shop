0595e09f2dc1ae5302a5273b2ad60c50c4fc8560
morteza078 <mortezarobatjazi.77@gmail.com>
2021-10-05 23:52:18 -0700

    add test for cart_detail url and login customer

--- customer/tests.py
+++ customer/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from customer.models import Customer
+
+
+class TestLoginView(TestCase):
+    def setUp(self):
+        customer=Customer.objects.create(username='test_user', email='test@gmail.com',)
+        customer.set_password('secret')
+    def test_login(self):
+        response = self.client.post('/en/customer/login/', {'username': 'morteza', 'password': 'secret'})
+        self.assertEqual(response.status_code, 200)
--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import  TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+    def test_cart_detail_view_status_code(self):
+        url = reverse('orders:cart_detail')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
