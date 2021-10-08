05e083e4f41eeacb2a61094c1af781697864d690
morteza078 <mortezarobatjazi.77@gmail.com>
2021-10-08 01:00:46 -0700

    change file name

--- shop/settings.py
+++ shop/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shop',
+        'NAME': 'shopi',
         'USER': 'postgres',
         'PASSWORD': 'Morteza078@',
         'HOST': '127.0.0.1',
